67bcb66d456ef48f7cc2d19671cccce1887c3511
leonreino <58917386+leonreino@users.noreply.github.com>
2021-11-04 13:09:22 +0300

    Implement get, add for transfer table and tuple to model functions

--- models/transfer_model.py
+++ models/transfer_model.py
@@ -13,19 +13,36 @@ class TransferModel:
         self.negative_reviews = negative_reviews
         self.is_accepted = is_accepted
 
+def create_models_from_tuple(transfers):
+    res_models = []
+    for t in transfers:
+        model = TransferModel(t[0], t[1], t[2], t[3], t[4], t[5], t[6], t[7], t[8], t[9])
+        res_models.append(model)
+    return res_models
+
 class TransferTable:
     def __init__(self, db_connection):
         self.db_connection = db_connection
         # self.tablename = "transfers"
 
     def get_all_transfers(self, limit=None):
-        pass
+        with self.db_connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM transfers ORDER BY offer_date DESC LIMIT %s", (limit,))
+            transfers = cursor.fetchall()
+
+        return create_models_from_tuple(transfers) 
 
     def get_transfer(self, transfer_id):
-        pass
+        with self.db_connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM transfers WHERE transfer_id = ?", transfer_id)
         
+
     def add_transfer(self, new_transfer:TransferModel):
-        pass
+        with self.db_connection.cursor() as cursor:
+            cursor.execute("INSERT INTO transfers (player_id, from_team, to_team, transfer_fee, is_loan, offer_date)\
+                            VALUES(?, ?, ?, ?, ?, ?)",
+                            new_transfer.player_id, new_transfer.from_team, new_transfer.to_team,
+                            new_transfer.transfer_fee, new_transfer.is_loan, new_transfer.offer_date)
 
     def delete_transfer(self, transfer_id):
         pass
