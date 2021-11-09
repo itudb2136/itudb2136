fca9be88ccf993023ea4845ba7ebcd1249e21805
leonreino <58917386+leonreino@users.noreply.github.com>
2021-11-09 21:11:36 +0300

    Add add_image function to image table and update model name

--- models/player_image_model.py
+++ models/player_image_model.py
@@ -1,4 +1,4 @@
-class PlayerImage:
+class PlayerImageModel:
     def __init__(self, player_id, image):
         self.player_id = player_id
         self.image = image
@@ -7,3 +7,9 @@ class PlayerImageTable:
     def __init__(self, db_connection):
         self.db_connection = db_connection
         # self.tablename = "players_image"
+
+    def add_image(self, i:PlayerImageModel):
+        with self.db_connection.cursor() as cursor:
+            cursor.execute("INSERT INTO players_image (id, image)\
+                            VALUES (%s, %s)",
+                            (i.player_id, i.image))
