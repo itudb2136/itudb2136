bd5be48bfce7a860315b288a3dcb0dd48969574b
leonreino <58917386+leonreino@users.noreply.github.com>
2021-11-09 20:57:01 +0300

    Add characteristics table with add function

--- models/player_main_characteristics_model.py
+++ models/player_main_characteristics_model.py
@@ -10,4 +10,13 @@ class PlayerCharacteristicsModel:
         self.defence_work_rate = defence_work_rate
 
 class PlayerCharacteristicsTable:
-    pass
+    def __init__(self, db_connection):
+        self.db_connection = db_connection
+        # self.tablename = "players_characteristics"
+
+    def add_characteristics(self, c:PlayerCharacteristicsModel):
+        with self.db_connection.cursor() as cursor:
+            cursor.execute("INSERT INTO players_characteristics (id, overall, potential, preferred_foot, weak_foot_skill, skill_moves, attack_work_rate, defence_work_rate)\
+                            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
+                            (c.player_id, c.overall, c.potential, c.preferred_foot, c.weak_foot_skill, c.skill_moves, c.attack_work_rate, c.defence_work_rate))
+            self.db_connection.commit()
