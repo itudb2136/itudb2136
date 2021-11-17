2ba046654f979ab6ae87622d457d0230b965020f
leonreino <58917386+leonreino@users.noreply.github.com>
2021-11-17 23:34:25 +0300

    Add transfer_controller with a testing route

---
+++ controllers/transfer_controller.py
@@ -0,0 +1,11 @@
+from flask import g, request, Blueprint, render_template
+from database import get_db
+from models.transfer_model import TransferModel, TransferTable
+
+transfer_bp = Blueprint('transfer_bp', __name__)
+
+@transfer_bp.route('/all_transfers', methods=['GET'])
+def show_all_transfers():
+    tt = TransferTable(get_db())
+    transfers = tt.get_all_transfers()
+    return render_template("test.html", transfers=transfers)
