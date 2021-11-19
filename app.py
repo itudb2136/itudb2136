fa40608a4f757a5b0de0ad6a951bcdb7cfdb2552
leonreino <58917386+leonreino@users.noreply.github.com>
2021-11-19 09:31:00 +0300

    Register player_controller and transfer_controller blueprints into app

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, g
 import config
 
-
 def create_app(debug=True):
     app = Flask(__name__)
     app.config.from_object(config.DevelopmentConfig)
@@ -9,6 +8,12 @@ def create_app(debug=True):
     with app.app_context():
         import database
         database.get_db()
+
+    from controllers.player_controller import player_bp
+    app.register_blueprint(player_bp)
+
+    from controllers.transfer_controller import transfer_bp
+    app.register_blueprint(transfer_bp)
 
     return app
 
