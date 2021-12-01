7359a7339216a3bc52ad59d544b0711ef8cb84d7
leonreino <58917386+leonreino@users.noreply.github.com>
2021-12-01 18:17:45 +0300

    Add blueprint for transfer controller

--- controllers/transfer_controller.py
+++ controllers/transfer_controller.py
@@ -2,7 +2,7 @@ from flask import g, request, Blueprint, render_template
 from database import get_db
 from models.transfer_model import TransferModel, TransferTable
 
-transfer_bp = Blueprint('transfer_bp', __name__)
+transfer_bp = Blueprint('transfer_bp', __name__, url_prefix="/transfers")
 
 @transfer_bp.route('/all_transfers', methods=['GET'])
 def show_all_transfers():
