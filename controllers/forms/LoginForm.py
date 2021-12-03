6a93cfbf4af87e9e6e4ab333edee671718ba3e46
leonreino <58917386+leonreino@users.noreply.github.com>
2021-12-03 21:33:54 +0300

    Add new LoginForm

--- controllers/forms/LoginForm.py
+++ controllers/forms/LoginForm.py
@@ -1,10 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, RadioField
-from wtforms.validators import DataRequired, EqualTo
+from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired(), EqualTo(fieldname='password_confirm', message="Passwords must be the same.")])
-    fullname = StringField('Full Name', validators=[DataRequired()])
-    password_confirm = PasswordField('Confirm Password')
-    role = RadioField("Role", choices=[("manager", "Manager"), ("fan", "Fan")], default="fan")
+    password = PasswordField('Password', validators=[DataRequired()])
+    
